992228794f69f567cf5cee45299b3aa27351f5fe
sunsolyee <gothf316@kookmin.ac.kr>
2024-06-02 16:58:50 +0900

    Delete wall directory

--- wall/gate.h
+++
@@ -1,7 +0,0 @@
-#include "Drawable.h"
-
-class Gate : public Drawable
-{
-public:
-    Gate(int y, int x);
-};
--- wall/wall.cpp
+++
@@ -1,3 +0,0 @@
-#include "Wall.h"
-Wall::Wall(int y, int x) : Drawable(y, x, '*') {}
-Immwall::Immwall(int y, int x):Drawable(y, x, '8') {}
